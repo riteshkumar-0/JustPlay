604e5349e976f69c973c18bbb8de78015bbcf318
Panda-rish <riteshkumar853892@gmail.com>
2024-06-12 21:51:41 +0530

    refresh & access token part

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { registerUser ,
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 router.route("/register").post(
@@ -18,8 +23,9 @@ router.route("/register").post(
     registerUser
     )
 
-// router.route("/login").post(loginUser)
-
+router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router
